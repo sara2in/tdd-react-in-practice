1ee3a46ff757011b6331183ffe4e8b6faefd77eb
Salem Ezz <sara2in@Salems-MacBook-Pro.local>
2022-02-22 17:47:27 -0500

    end of day, sry did push on evey test, happy this is practice.

--- cypress/integration/home/home.spec.js
+++ cypress/integration/home/home.spec.js
@@ -1,11 +1,57 @@
 describe("Home page", () => {
     beforeEach(() => {
         cy.visit('/')
-    })
-    it("header contains recipe heading with a message that there are no recipes", () => {
+      })
+  it("header contains recipe heading with a message that there are no recipes", () => {
       cy.findByRole('heading').should('contain', 'My Recipes')
       cy.get('p')
         .findByText('There are no recipes to list.')
         .should('exist')
     })
+
+  it("contains an add recipe button that when clicked opens a form", () => {
+    cy.findByRole('button').click();
+  
+    cy.get('form')
+      .findByRole('button')
+      .should('exist')
   })
+
+  it("contains a form with fields 'Recipe Name' and 'Recipe Instructions' after clicking the 'Add Recipe' button", () => {
+    cy.findByRole('button').click();
+    expect(cy.findByRole('textbox', {name: /Recipe name/i})).toExist()
+    cy.findByRole('textbox', {name: /instructions/i}).should('exist')
+  })
+
+  it("displays a recipe name under the 'My Recipes' heading after it has been added through the 'Add Recipe' form", () => {
+    const recipeName = 'Tofu Scramble Tacos';
+    cy.findByRole('button').click()
+    cy.findByRole('textbox', {name: /Recipe name/i}).type(recipeName)
+    cy.findByRole('textbox', {name: /instructions/i}).type("1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas")
+  
+    return cy.findByRole('button').click()
+      .then(() => {
+      expect(cy.findByRole('listitem', /tofu scramble tacos/i)).toExist();
+      })   
+  })
+
+  it("adds multiple recipes", () => {
+    const recipeName1 = 'Lean Pockets';
+    cy.findByRole('button').click()
+    cy.findByRole('textbox', {name: /Recipe name/i}).type(recipeName1)
+    cy.findByRole('textbox', {name: /instructions/i}).type("place in toaster oven on 350 for 45 minutes")
+
+    cy.findByRole('button').click()
+
+    const recipeName2 = 'Ramen Noodles';
+    cy.findByRole('button').click()
+    cy.findByRole('textbox', {name: /Recipe name/i}).type(recipeName2)
+    cy.findByRole('textbox', {name: /instructions/i}).type("Pour hot water in cup and let sit for 3 minutes.")
+  
+    return cy.findByRole('button').click()
+      .then(() => {
+      expect(cy.findByRole('list')).toHaveLength(2);
+      //expect(cy.findByRole('listitem', /Lean Pockets/i)).toExist();
+      })   
+  })
+})
--- src/App.js
+++ src/App.js
@@ -1,13 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  state = {
+    isAddRecipeFormDisplayed: false,
+    recipes: [],
+    newRecipeName: "",
+    newRecipeInstructions: ""
+  }
+  
+  // handleRecipeInstructionsChange = (event) => {
+  //   const value = event.target.value;
+  
+  //   this.setState({newRecipeInstructions: value});
+  // }
+
+  // handleRecipeNameChange = (event) => {
+  //   const value = event.target.value;
+  
+  //   this.setState({newRecipeName: value});
+  // }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+  
+    this.setState({[name]: target.value});
+  }
+
+  toggleAddRecipeForm = () => {
+    this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
+  }
+
+  submitRecipe = (event) => {
+    event.preventDefault()
+    this.setState({recipes: [
+        ...this.state.recipes,
+        {
+          name: this.state.newRecipeName,
+          instructions :this.state.newRecipeInstructions
+        }
+      ]
+    })
+    this.setState( {    newRecipeName: "",
+    newRecipeInstructions: ""} )
+    this.toggleAddRecipeForm()
+  }
+
+  render(){
+  const addNewRecipeForm = (
+      <form id="recipe-form" onSubmit={this.submitRecipe} >
+        <label htmlFor="newRecipeName">Recipe name: </label>
+        <input type="text"
+          id="newRecipeName"
+          name="newRecipeName"
+          onChange={this.handleChange}
+          value={this.state.newRecipeName} />
+        <label htmlFor="newRecipeInstructions">Instructions:</label>
+        <textarea id="newRecipeInstructions"
+          name="newRecipeInstructions"
+          placeholder="write recipe instructions here..."
+          onChange={this.handleChange}
+          value={this.state.newRecipeInstructions} />
+        <input type="submit" />
+      </form>
+    )
+    const recipeList = this.state.recipes.map(recipe => {
+      return <li>{recipe.name}</li>
+    })
+    return (
+      <div className="App">
       <h1 className="App-header">My Recipes</h1>
-      <p>There are no recipes to list.</p>
+      {
+        this.state.isAddRecipeFormDisplayed
+        ? addNewRecipeForm
+        : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
+      }
+      {
+        this.state.recipes.length > 0 ?
+        <ul>
+          { recipeList }
+        </ul> :
+        <p>There are no recipes to list.</p>
+      }
     </div>
-  );
+    )
+  }
 }
 
 export default App;
